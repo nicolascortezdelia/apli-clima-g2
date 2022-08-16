6e03162e910c31b3151ca66fcf8eed5d4facd551
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2022-08-16 20:41:34 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [clima, setClima] = useState();
+
+  const handleClima = (id) => {
+    setClima(id);
+  };
+
+  const getApi = async () => {
+    const { data } = await axios('http://api.weatherapi.com/v1/search.json?key=6be8c28794924ed8a2a184922222905&q=tuc');
+    console.log({ data });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hola mundo</h1>
+      <button className='btn btn-primary' onClick={getApi} key="6be8c28794924ed8a2a184922222905"> Traer Clima</button>
+
     </div>
   );
 }
