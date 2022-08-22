1cd88f8c9804f4a541b67d59a0b636533bae86a9
ceci dip <cecidip41@gmail.com>
2022-08-22 19:06:51 -0300

    agrego estilos para maqueta

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import Clima from './components/Card/Clima';
+import { Header } from './components/Header';
+import { Search } from './components/Search';
+import './style/style.css';
 
 function App() {
   return (
-    <Clima />
+    <div className="container">
+      <div className="row">
+        <div className="col col-12"><Header/></div>
+        <div className="col col-6"><Clima/></div>
+        <div className="col-6"><Search/></div>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <h1 className='text-center my-3'>Clima por ciudades</h1>
+  );
+};
+
+export default Header;
