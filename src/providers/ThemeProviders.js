0b0046c750892efb215007ec2a08879923841049
ceci dip <cecidip41@gmail.com>
2022-08-22 20:09:29 -0300

    custom hooks use context

--- src/providers/ThemeProviders.js
+++ src/providers/ThemeProviders.js
@@ -3,8 +3,22 @@ import { createContext, useContext, useState } from 'react';
 const themeContext = createContext();
 const themeToggleContext = createContext();
 
-export const ThemeProvider = () => {
+export const useThemeContext = () => {
+  return useContext(themeContext);
+};
+
+export const useThemeToggleContext = () => {
+  return useContext(themeToggleContext);
+};
+
+export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState('light');
 
-  return { theme, setTheme };
+  return (
+    <themeContext.Provider value={theme}>
+        <themeToggleContext.Provider value={setTheme}>
+            {children}
+        </themeToggleContext.Provider>
+    </themeContext.Provider>
+  );
 };
